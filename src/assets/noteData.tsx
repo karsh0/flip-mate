e1fc013b32718afa28ad6a2682c5daeda2a87088
Karan <karansharma192004@gmail.com>
2025-05-07 21:43:11 +0530

    comments

--- src/assets/noteData.tsx
+++ src/assets/noteData.tsx
@@ -19,31 +19,31 @@ import Note from "../components/note";
       title: "Designers",
       description:
         "Move your design swipe file to Clipmate and watch as Clipmate organizes your content automatically.",
-      icon: <FolderKanban color="rgb(245, 55, 12)" />, // design organization
+      icon: <FolderKanban color="rgb(245, 55, 12)" />, 
     },
     {
       title: "Researchers",
       description:
         "Save and analyse papers, videos, news articles in the moment and without inconvenient context-switching.",
-      icon: <BookOpenCheck color="rgb(245, 55, 12)" />, // research/reading
+      icon: <BookOpenCheck color="rgb(245, 55, 12)" />, 
     },
     {
       title: "Developers",
       description:
         "Find that one Github repository that you bookmarked two weeks ago and promised you'd come back to.",
-      icon: <Code2 color="rgb(245, 55, 12)" />, // coding/dev
+      icon: <Code2 color="rgb(245, 55, 12)" />, 
     },
     {
       title: "Marketers",
       description:
         "Drowning in campaign ideas and market trends? Clipmate helps you organize insights and keep your strategy sharp and accessible.",
-      icon: <Megaphone color="rgb(245, 55, 12)" />, // marketing/speaking
+      icon: <Megaphone color="rgb(245, 55, 12)" />, 
     },
     {
       title: "Entrepreneurs",
       description:
         "Entrepreneurs always have too much going on so who's ever going to read the endless read-it-later pile. No one! But what if you don't have to read it?",
-      icon: <Lightbulb color="rgb(245, 55, 12)" />, // ideas/startups
+      icon: <Lightbulb color="rgb(245, 55, 12)" />, 
     },
   ];
   
